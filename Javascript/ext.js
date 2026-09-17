40148d39e52247261f2fe8329b82643a1008d266
Pravin Kalale <kalalepravin2@gmail.com>
2026-09-17 09:39:02 +0530

    Add BOM concepts

--- Javascript/ext.js
+++ Javascript/ext.js
@@ -2,3 +2,18 @@ function msg()
 {
     alert("Hello Javascript");
 }
+
+function test()
+        {
+            document.write("hello good morning");
+        }
+
+        function para(a,b)
+        {
+            document.write("Addition of two numbers: ",a+b);
+        }
+
+        function square(num)
+        {
+            return num*num;
+        }
